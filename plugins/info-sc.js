55c71f770e6678fd32b34afbef6d8b61318718cc
Reiibotz <topuprainalz@gmail.com>
2022-11-11 14:53:02 +0700

    first commit

---
+++ plugins/info-sc.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, participants, usedPrefix: _p }) => {
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = conn.getName(who)
+	
+                    let wan = 'https://telegra.ph/file/c080917609684bf229cd3.jpg'
+let anu2 =`Esceh?\nNih:`
+  conn.sendButtonImg(m.chat, await(await fetch(wan)).buffer(), anu2, botdate, 'Owner', 'owner',)
+}
+handler.help = ['sc']
+handler.tags = ['info']
+handler.command = /^(sc|script|esceh)$/i
+handler.group = false
+handler.limit = false
+
+handler.fail = null
+
+export default handler
